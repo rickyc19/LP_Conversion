2ea52ffcf64527b2ef8caec582e2cb84eb27f1c4
rickyc19 <64217474+rickyc19@users.noreply.github.com>
2020-04-24 12:29:21 -0500

    renamed file

--- color_detect.py
+++ color_detect.py
@@ -1,37 +1,27 @@
-import pygsheets
-import phonenumbers
-from urlextract import URLExtract
-import pyap
-import os
+def create_matrix(cells):
+    # Create the final sheet we will eventually update back. Headers are just an example at this point.
+    final_sheet = [['Program Name:', 'Address 1', 'Address 2', 'Phone 1', 'Phone 2', 'Fax', 'URL', 'Email', 'Notes']]
 
-client = pygsheets.authorize(client_secret='oauth.json')
-spreadsheet = \
-    client.open_by_url("https://docs.google.com/spreadsheets/d/1CIpWBZXAK_PqsEq2vLlFklT2MgQjH008qbxgPYXbRtU/edit#gid=0")
-sheet = spreadsheet.sheet1
+    # Remove cells from list until we get to the first highlighted cell, ie the first program
+    while cells[0].color != (0.8509804, 0.91764706, 0.827451, 0):
+        cells.pop(0)
 
-cells = sheet.get_col(1, include_tailing_empty=False, returnas='cells')
+    # Row tracks which row of the final sheet we are working on.
+    row = 0
 
-# Create the final sheet we will eventually update back. Headers are just an example at this point.
-final_sheet = [['Program Name:', 'Address 1', 'Address 2', 'Phone 1', 'Phone 2', 'Fax', 'URL', 'Email', 'Notes']]
-
-# Remove cells from list until we get to the first highlighted cell, ie the first program
-while cells[0].color != (0.8509804, 0.91764706, 0.827451, 0):
-    cells.pop(0)
-
-# Row tracks which row of the final sheet we are working on.
-row = 0
-
-# For each cell: highlighted ones are a new row for final sheet, others are information for the 2nd cell of same row.
-for c in cells:
-    if c.color == (0.8509804, 0.91764706, 0.827451, 0):
-        if final_sheet[row][1]:
-            final_sheet.append([c.value, ''])
-            row += 1
-        # If we haven't added any information on the program yet (even a blank row) then this line must be more title
+    # For each cell: highlighted ones are a new row for final sheet, others are information for the 2nd cell of same row.
+    for c in cells:
+        if c.color == (0.8509804, 0.91764706, 0.827451, 0):
+            if final_sheet[row][1]:
+                final_sheet.append([c.value, ''])
+                row += 1
+            # If we haven't added any information on the program yet (even a blank row) then this line must be more title
+            else:
+                final_sheet[row][0] += ' ' + c.value
+            # TODO: Distinguish between consecutive single line programs and titles split over multiple lines
         else:
-            final_sheet[row][0] += ' ' + c.value
-        # TODO: Distinguish between consecutive single line programs and titles split over multiple lines
-    else:
-        if final_sheet[row][1]:
-            final_sheet[row][1] += ', '
-        final_sheet[row][1] += c.value
+            if final_sheet[row][1]:
+                final_sheet[row][1] += ', '
+            final_sheet[row][1] += c.value
+
+    return final_sheet
--- list_conversion.py
+++ list_conversion.py
@@ -1,41 +1,13 @@
 import pygsheets
-import phonenumbers
-from urlextract import URLExtract
-import pyap
-import os
+from color_detect import *
+from matrix_detect import *
 
 client = pygsheets.authorize(client_secret='oauth.json')
 spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1CIpWBZXAK_PqsEq2vLlFklT2MgQjH008qbxgPYXbRtU/edit#gid=0")
 sheet = spreadsheet.sheet1
-
 cells = sheet.get_col(1, include_tailing_empty=False, returnas='cells')
-phone_string = ""
-web_string = ""
-add_match = ""
-add_string = ""
 
-data_matrix = [[]]
-data_matrix.clear()
-value_list = list()
-
-# Add cell values to list
-for c in cells:
-    v = c.value
-    value_list.append(v)
-
-for l in value_list:
-
-    try: add_string = pyap.parse(l, country='US')[0].__str__()  # Get address from list value
-    except: add_string = ""
-
-    try: web_string = URLExtract().find_urls(l)[0]  # Get URL from list value
-    except: web_string = ""
-
-    for match in phonenumbers.PhoneNumberMatcher(l, "US"):  # Get phone number from list value
-        phone_string = phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.NATIONAL)
-
-    # Add data to matrix
-    data_matrix.append([phone_string, web_string, add_string])
-    phone_string = ""
-
-sheet.update_values("B:D", data_matrix)  # Update cell range
+# Create data matrix from cells
+final_sheet = create_matrix(cells)
+# Get necessary info from data matrix and put in correct columns
+find_info(final_sheet, sheet)
