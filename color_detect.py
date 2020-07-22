e70a59f4e68e68b946f912d67ab39775c963b218
rickyc19 <rickyc19live@gmail.com>
2020-07-22 14:44:31 -0500

    - Split detection into 2 functions, bold or every row

--- color_detect.py
+++ color_detect.py
@@ -1,19 +1,29 @@
-def create_matrix(cells):
-    # Create the final sheet we will eventually update back. Headers are just an example at this point.
-    final_sheet = [['Program Name:', 'Address 1', 'Address 2', 'Phone 1', 'Phone 2', 'Fax', 'URL', 'Email', 'Notes']]
-
-    # Remove cells from list until we get to the first highlighted cell, ie the first program
-    while cells[0].color != (0.8509804, 0.91764706, 0.827451, 0):
-        cells.pop(0)
-
-    # Row tracks which row of the final sheet we are working on.
+# Every row is added to both columns of matrix
+def every_row(cells):
+    final_sheet = [['Program Name', 'Address 1', 'Address 2', 'Phone 1', 'Phone 2', 'Fax', 'URL', 'Email', 'Notes']]
     row = 0
 
-    # For each cell: highlighted ones are a new row for final sheet, others are information for the 2nd cell of same row.
     for c in cells:
-        if c.color == (0.8509804, 0.91764706, 0.827451, 0):
+        final_sheet.append([c.value, ' '])
+        final_sheet[row][1] += c.value
+        row += 1
+
+    return final_sheet
+
+
+# For each cell: bolded ones are a new row for final sheet, others are information for the 2nd cell of same row.
+def bold(cells):
+    final_sheet = [['Program Name', 'Address 1', 'Address 2', 'Phone 1', 'Phone 2', 'Fax', 'URL', 'Email', 'Notes']]
+    row = 0
+
+    for c in cells:
+
+        cj = c.get_json()
+
+        if 'bold' in cj['userEnteredFormat']['textFormat']:
+            # if cj['userEnteredFormat']['textFormat']['fontSize'] == 21:
             if final_sheet[row][1]:
-                final_sheet.append([c.value, ''])
+                final_sheet.append([c.value, ' '])
                 row += 1
             # If we haven't added any information on the program yet (even a blank row) then this line must be more title
             else:
@@ -21,8 +31,9 @@ def create_matrix(cells):
             # TODO: Distinguish between consecutive single line programs and titles split over multiple lines
         else:
             if final_sheet[row][1]:
-                final_sheet[row][1] += ', '
+                final_sheet[row][1] += ' '
             final_sheet[row][1] += c.value
 
     return final_sheet
+
 
--- list_conversion.py
+++ list_conversion.py
@@ -2,7 +2,7 @@ import pygsheets
 from color_detect import *
 from matrix_detect import *
 
-client = pygsheets.authorize(client_secret='oauth.json')
+client = pygsheets.authorize(client_secret='OAuth.json')
 spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1CIpWBZXAK_PqsEq2vLlFklT2MgQjH008qbxgPYXbRtU/edit#gid=0")
 sheet = spreadsheet.sheet1
 cells = sheet.get_col(1, include_tailing_empty=False, returnas='cells')
